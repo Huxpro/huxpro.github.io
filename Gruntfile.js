fd7048639b5b5eb85daba9ef2a05c9afb58d43a4
鬼栈 <huangxuan.hx@alibaba-inc.com>
2015-02-02 02:38:27 +0800

    fix NPM so i can use Grunt and Less now, Social-Comments feature done: use Duoshuo

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,6 @@ module.exports = function(grunt) {
         banner: '/*!\n' +
             ' * <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
             ' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
-            ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n' +
             ' */\n',
         usebanner: {
             dist: {
