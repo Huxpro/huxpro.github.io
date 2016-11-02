4bb4180e7b0c51d3245cafd4097bc82e9bb77087
黄玄 <huxpro@gmail.com>
2016-11-02 19:01:59 +0800

    [self] Unresponsiblie SW Test...please

--- sw.js
+++ sw.js
@@ -15,6 +15,40 @@ const HOSTNAME_WHITELIST = [
   "cdnjs.cloudflare.com"
 ]
 
+
+// The Util Function to hack URLs of intercepted requests
+const getFixedUrl = (req) => {
+  var now = Date.now();
+  url = new URL(req.url)
+
+  // 1. fixed http URL
+  // Just keep syncing with location.protocol 
+  // fetch(httpURL) belongs to active mixed content. 
+  // And fetch(httpRequest) is not supported yet.
+  url.protocol = self.location.protocol
+
+  // 2. add query for caching-busting.
+  // Github Pages served with Cache-Control: max-age=600
+  // max-age on mutable content is error-prone, with SW life of bugs can even extend.
+  // Until cache mode of Fetch API landed, we have to workaround cache-busting with query string.
+  // Cache-Control-Bug: https://bugs.chromium.org/p/chromium/issues/detail?id=453190
+  url.search += (url.search ? '&' : '?') + 'cache-bust=' + now;
+  return url.href
+}
+
+// The Util Function to filter out Navigation Requests.
+// request.mode of 'navigate' is unfortunately not supported in Chrome
+// versions older than 49, so we need to include a less precise fallback,
+// which checks for a GET request with an Accept: text/html header.
+const isNavigationReq = (req) => (req.mode === 'navigate' || (req.method === 'GET' && req.headers.get('accept').includes('text/html')))
+
+// Redirect in SW manually fixed github pages 404s on repo?blah 
+// if An url is navigation and isnt end with '/' (indicate github would 404s on it )
+// it should be a dir request and need to fixed
+// P.S. An url.pathname has no '.' cant indicate it's file (e.g. http://test.com/api/version/1.2/)
+// https://twitter.com/Huxpro/status/793519812127227905
+const shouldRedirect = (req) => (isNavigationReq(req) && req.url.substr(-1) !== "/")
+
 /**
  *  @Lifecycle Install
  *  Precache anything static to this version of your app.
@@ -53,40 +87,48 @@ self.addEventListener('activate',  event => {
  *  void respondWith(Promise<Response> r);
  */
 self.addEventListener('fetch', event => {
+  // logs for debugging
+  console.log(`fetch ${event.request.url}`)
+  //console.log(` - type: ${event.request.type}; destination: ${event.request.destination}`)
+  //console.log(` - mode: ${event.request.mode}, accept: ${event.request.headers.get('accept')}`)
+
   // Skip some of cross-origin requests, like those for Google Analytics.
-  console.log("fetch", event.request.url)
   if (HOSTNAME_WHITELIST.indexOf(new URL(event.request.url).hostname) > -1) {
+    
     // Stale-while-revalidate 
     // similar to HTTP's stale-while-revalidate: https://www.mnot.net/blog/2007/12/12/stale
-    event.respondWith(
-      caches.open(RUNTIME).then(cache => {
-        return caches.match(event.request).then(cachedResponse => {
-          // fetch(httpURL) is active mixed content, fetch(httpRequest) is not supported yet...
-          var fixedUrl = event.request.url.replace('http://', '//')
-          // cache busting
-          var fetchPromise = fetch(`${fixedUrl}?${Math.random()}`,  {
-            cache: "no-store",
-            redirect: "follow"
-          })
-            .then(networkResponse => {
-              // Fetch API not throw error on 404, 500 etc
-              if (!networkResponse.ok) throw Error('Not ok');
-              var resUrl = networkResponse.url
-              cache.put(event.request, networkResponse.clone())
-              return networkResponse;
-            })
-            .catch(error => {
-              console.log(error);
-              // for navigation requests, fallback to offline.html
-              if(event.request.url.substr(-1) == "/") {
-                return caches.match('offline.html')
-              }
-            })
-        
-          // Return the response from cache or wait for network.
-          return cachedResponse || fetchPromise;
+    // Upgrade from Jake's to Surma's: https://gist.github.com/surma/eb441223daaedf880801ad80006389f1
+    const cached = caches.match(event.request);
+    const fixedUrl = getFixedUrl(event.request);
+    const fetched = fetch(fixedUrl, {cache: "no-store"});
+    const fetchedCopy = fetched.then(resp => resp.clone());
+
+    // Redirect in SW manually fixed github pages 404s on repo?blah 
+    if(shouldRedirect(event.request)){
+      event.respondWith(
+        new Response(null, {
+          "status": 301,
+          "headers": {"location": `${event.request.url}/`}
         })
-      })
+      )
+      return;
+    }
+
+    // Call respondWith() with whatever we get first.
+    // If the fetch fails (e.g disconnected), wait for the cache.
+    // If there’s nothing in cache, wait for the fetch. 
+    // If neither yields a response, return offline pages.
+    event.respondWith(
+      Promise.race([fetched.catch(_ => cached), cached])
+        .then(resp => resp || fetched)
+        .catch(_ => caches.match('offline.html'))
+    );
+
+    // Update the cache with the version we fetched (only for ok status)
+    event.waitUntil(
+      Promise.all([fetchedCopy, caches.open(RUNTIME)])
+        .then(([response, cache]) => response.ok && cache.put(event.request, response))
+        .catch(_ => {/* eat any errors */})
     );
   }
 });
